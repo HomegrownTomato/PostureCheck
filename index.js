873675cabd21498d5cec862f16fb03de4eef6e5c
HomegrownTomato <tomatohomegrown@gmail.com>
2023-11-11 12:41:59 -0600

    First and only commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+function sendMessage() {
+  const date = new Date();
+  const dayOfWeek = date.getDay();
+  const hour = date.getHours();
+
+  //This check is here to make sure it only runs during business hours.
+  if (dayOfWeek === 0 || dayOfWeek === 6 || hour < 9 || hour >= 17) {
+    return;
+  }
+
+  const message = {
+    content: "Check Your Posture"
+  };
+  axios.post(" Put your discord Webhook Url Here", message)
+  .then(response => {
+    console.log(`Message sent at ${date.toLocaleString()}`);
+  })
+  .catch(error => {
+    console.error(`Error sending message at ${date.toLocaleString()}: ${error.message}`);
+  });
+}
+
+// Set the interval to every 3 hours
+setInterval(sendMessage, 3 * 60 * 60 * 1000);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
